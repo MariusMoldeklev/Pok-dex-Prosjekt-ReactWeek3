55a4819d85ce191529b048fa10962711cea13ba7
Mariusm.koder <mariusm.koder@gmail.com>
2024-03-20 11:23:53 +0100

    local host up and running

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,4 @@
-import { EventHandling } from "../componets/EventHandling/EventHandling";
-import { InlineEventHandling } from "../componets/InlineEventHandling/InlineEventHandling";
-import { InputText } from "../componets/InputText/InputText";
-import { MouseTracker } from "../componets/MouseTracker/MouseTracker";
-import { Counter } from "../componets/counter/Counter";
+import { Counter } from "../components/Counter";
 
 export function HomePage() {
   const title = "Home Page"
@@ -11,25 +7,6 @@ export function HomePage() {
     <main style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <h1>{title}</h1>
 
-      <section>
-        <h2>Innline Event Handling</h2>
-        <InlineEventHandling />
-      </section>
-
-      <section>
-        <h2>Event Handling</h2>
-        <EventHandling />
-      </section>
-
-      <section>
-        <h2>Input Text</h2>
-        <InputText />
-      </section>
-
-      <section>
-        <h2>Mouse Tracker</h2>
-        <MouseTracker />
-      </section>
 
       <section>
         <h2>Counter</h2>
