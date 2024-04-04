0487369b84c97791783c89f6344a26f1dbc0e48e
Mariusm.koder <mariusm.koder@gmail.com>
2024-04-04 11:33:42 +0200

    poke search added and poke details added /w routes in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Switch,
 } from 'react-router-dom'
 import './App.css'
 import { Layout } from './pages/Layout'
 import { HomePage } from './pages/HomePage'
 import { AboutPage } from './pages/about/AboutPage'
 import { NextPage } from './pages/next/NextPage'
+import { PokemonDetail } from './pages/pokemonDetail/pokemonDetail'
+import { PokemonSearch } from './components/Search'
 
 function App() {
   return (
@@ -22,6 +25,10 @@ function App() {
           {/* path blir her parent route + this path => "/" + "about" = "/about" */}
           <Route path='about' element={<AboutPage />} />
           <Route path='next' element={<NextPage />} />
+          <Switch>
+        <Route path="/" exact component={PokemonSearch} />
+        <Route path="/pokemon/:name" component={PokemonDetail} />
+      </Switch>
         </Route>
 
       </Routes>
